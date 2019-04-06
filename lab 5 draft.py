694366e328f9f12c8d4fe7c5fcd736e9b7dd2a7e
JustinDRui <45852013+JustinDRui@users.noreply.github.com>
2019-04-05 20:54:45 -0600

    Add files via upload

---
+++ lab 5 draft.py
@@ -0,0 +1,161 @@
+#Jusitn Ruiloba
+#lab 5
+#CS2302 Data structures
+#4/5/19
+class HashTableC(object):
+    # Builds a hash table of size 'size'
+    # Item is a list of (initially empty) lists
+    # Constructor
+    def __init__(self,size):  
+        self.item = []
+        self.num_items= size
+        for i in range(size):
+            self.item.append([])
+        
+def InsertC(H,k,l):
+    # Inserts k in appropriate bucket (list) 
+    # Does nothing if k is already in the table
+    b = h(k,len(H.item))
+    H.item[b].append([k,l]) 
+   
+def FindC(H,k):
+    # Returns bucket (b) and index (i) 
+    # If k is not in table, i == -1
+    b = h(k,len(H.item))
+    for i in range(len(H.item[b])):
+        if H.item[b][i][0] == k:
+            return b, i, H.item[b][i][1]
+    return b, -1, -1
+ 
+def h(s,n):
+    r = 0
+    for c in s:
+        r = (r*255 + ord(c))% n
+    return r
+
+
+
+#######################################
+########################
+#binary tree
+# Code to implement a binary search tree 
+# Programmed by Olac Fuentes
+# Last modified February 27, 2019
+
+class BST(object):
+    # Constructor
+    def __init__(self, item, left=None, right=None):  
+        self.item = item
+        self.left = left 
+        self.right = right      
+        
+def Insert(T,newItem):
+    if T == None:
+        T =  BST(newItem)
+    elif T.item > newItem:
+        T.left = Insert(T.left,newItem)
+    else:
+        T.right = Insert(T.right,newItem)
+    return T
+
+def Delete(T,del_item):
+    if T is not None:
+        if del_item < T.item:
+            T.left = Delete(T.left,del_item)
+        elif del_item > T.item:
+            T.right = Delete(T.right,del_item)
+        else:  # del_item == T.item
+            if T.left is None and T.right is None: # T is a leaf, just remove it
+                T = None
+            elif T.left is None: # T has one child, replace it by existing child
+                T = T.right
+            elif T.right is None:
+                T = T.left    
+            else: # T has two chldren. Replace T by its successor, delete successor
+                m = Smallest(T.right)
+                T.item = m.item
+                T.right = Delete(T.right,m.item)
+    return T
+         
+def InOrder(T):
+    # Prints items in BST in ascending order
+    if T is not None:
+        InOrder(T.left)
+        print(T.item,end = ' ')
+        InOrder(T.right)
+  
+def InOrderD(T,space):
+    # Prints items and structure of BST
+    if T is not None:
+        InOrderD(T.right,space+'   ')
+        print(space,T.item)
+        InOrderD(T.left,space+'   ')
+  
+def SmallestL(T):
+    # Returns smallest item in BST. Returns None if T is None
+    if T is None:
+        return None
+    while T.left is not None:
+        T = T.left
+    return T   
+ 
+def Smallest(T):
+    # Returns smallest item in BST. Error if T is None
+    if T.left is None:
+        return T
+    else:
+        return Smallest(T.left)
+
+def Largest(T):
+    if T.right is None:
+        return T
+    else:
+        return Largest(T.right)   
+
+def Find(T,k):
+    # Returns the address of k in BST, or None if k is not in the tree
+    if T is None or T.item == k:
+        return T
+    if T.item<k:
+        return Find(T.right,k)
+    return Find(T.left,k)
+    
+def FindAndPrint(T,k):
+    f = Find(T,k)
+    if f is not None:
+        print(f.item,'found')
+    else:
+        print(k,'not found')
+    
+# Code to test the functions above
+T = None
+A = [70, 50, 90, 130, 150, 40, 10, 30, 100, 180, 45, 60, 140, 42]
+for a in A:
+    T = Insert(T,a)
+    
+InOrder(T)
+print()
+
+
+################
+
+################################
+#hash
+
+# Implementation of hash tables with chaining using strings
+option=input("Binary tree or hash table")
+f = open("text.txt", "r")
+print(f.readline())
+print(f.readline())
+
+
+if option == "hashtable":
+  H = HashTableC(11)
+  A = ['data','structures','computer','science','university','of','texas','at','el','paso']
+  for a in A:
+    InsertC(H,a,alen(a))
+    print(H.item)
+
+  for a in A: # Prints bucket, position in bucket, and word length
+    print(a,FindC(H,a))
+ 
